5dfa868702e21959b4065bf5c6693c0036aea02a
Vikash Kumawat <75132736+vikashjalandra@users.noreply.github.com>
2024-07-19 18:08:03 +0530

    api utils

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import dotenv from 'dotenv';
+import app from './app.js';
 
 import express from 'express';
 import connectDB from './db/conn.js';
 
 dotenv.config({ path: './env' });
 
-connectDB(); 
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT ||8000,()=>{
+        console.log(`Server running port${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log(err);
+})
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+const asyncHandler =(requestHandler)=>{
+    (req, res, next)=>{
+        Promise.resolve(requestHandler(req, res, next)).catch((err)=>next(err))
+    }
+}
+
+
+
+export {asyncHandler}
+
+// Method 2
+// const asyncHandler =(fn)=> async(req, res, next)=>{
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success:false,
+//             message:error.message
+//         })
+//     }
+// }
