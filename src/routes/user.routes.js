85b7d6809f7e01e7d36165f3f567afcec5305a2d
Vikash Kumawat <75132736+vikashjalandra@users.noreply.github.com>
2024-07-24 11:43:27 +0530

    new

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,6 +3,5 @@ import { registerUser } from "../controllers/user.controller.js";
 
 const router = Router()
 router.route("/register").post(registerUser)
-router.route("/login").post(loginUser)
 
 export default router;
