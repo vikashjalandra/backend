6957fffa519b9c709f6d7c0ce04cb4e86fc61542
Vikash Kumawat <75132736+vikashjalandra@users.noreply.github.com>
2024-08-01 18:31:53 +0530

    feat: Add getWatchHistory endpoint for retrieving user's watch history

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from '../models/user.model.js'
 import { uploadOnCloudinary } from '../utils/cloudinary.js'
 import { ApiResponse } from '../utils/ApiResponse.js'
 import JWT from 'jsonwebtoken'
+import mongoose from 'mongoose'
 
 const generateAccessAndRefreshTokens = async (userId) => {
     try {
@@ -358,6 +359,51 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
     .json(
         new ApiResponse(200,channel[0],"User channel fetched successfully.")
     )
+})
+
+const getWatchHistory = asyncHandler(async(req,res)=>{
+    const user = await User.aggregate([
+        {
+            $match:{
+                _id:new  mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+           $lookup:{
+            from:"videos",
+            localField:"watchHistory",
+            foreignField:"_id",
+            as:"watchHistory",
+            pipeline:[
+                {
+                    $lookup:{
+                        from:"users",
+                        localField:"owner",
+                        foreignField:"_id",
+                        as:"owner",
+                        pipeline:[
+                            {
+                                $project:{
+                                    fullname:1,
+                                    username:1,
+                                    avatar:1,
+                                    coverImage:1
+                                }
+                            },
+                            {
+                                $addFields:{
+                                    owner:{
+                                        $first:"$owner"
+                                    }
+                                }
+                            }
+                        ]
+                    }
+                }
+            ]
+           } 
+        }
+    ])
 })
 
 export {
