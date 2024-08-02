1f879406f3201b0d749fd9a49df7c503ec5444a5
Vikash Kumawat <75132736+vikashjalandra@users.noreply.github.com>
2024-08-02 15:32:04 +0530

    feat: Add getWatchHistory and getWatchHistory endpoints

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -404,6 +404,8 @@ const getWatchHistory = asyncHandler(async(req,res)=>{
            } 
         }
     ])
+
+    return res.status(200).json(new ApiResponse(200,user[0].watchHistory,"Watch history fetched successfully."))
 })
 
 export {
@@ -417,4 +419,5 @@ export {
     updateUserAvatar,
     updateUserCoverImage,
     getUserChannelProfile,
+    getWatchHistory,
 }
